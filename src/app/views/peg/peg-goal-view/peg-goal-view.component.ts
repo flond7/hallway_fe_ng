09cd687b4718e2c26f57d06bb9c97d05c00f4cd5
flond7 <elisa.utilivenza@gmail.com>
2023-03-23 09:47:33 +0100

    real data and progress calculation

--- src/app/views/peg/peg-goal-view/peg-goal-view.component.ts
+++ src/app/views/peg/peg-goal-view/peg-goal-view.component.ts
@@ -8,18 +8,36 @@ import { Component } from '@angular/core';
 export class PegGoalViewComponent {
 
 obj = {
+  name: "Predisposizione PIAO",
+  boss: "Dott.ssa Patrizia Mascellino",
   type: "ordinario",
+  weight: 1,
   start: "",
   end: "",
   office: "",
   year: "",
   people: ["Elisa Pessa", "Elisa Pessa", "Elisa Pessa"],
   phases: [
-    {description: "description 1", weight: "50", status:"fatto"},
-    {description: "description 2", weight: "30", status:"fatto"},
-    {description: "description 3", weight: "20", status: "in corso"},
-    {description: "description 2", weight: "30", status:"da fare"},
+    {description: "Studio della composizione del Piano e stesura parte relativa al Piano Nazionale Anticorruzione", startPrevision: "01/01/2023", endPrevision:"01/01/2023", weight: "40", startReal:"", endReal:"", status:"fatto", onTime: true},
+    {description: "Redazione parte relativa al Piano della Performance e piano della formazione", startPrevision: "13/02/2023", endPrevision:"20/03/2023", weight: "40", startReal:"", endReal:"", status:"fatto", onTime: true},
+    {description: "Predisposizione atto finale e approvazione in Giunta. Successivi adempimenti.", startPrevision: "13/02/2023", endPrevision:"20/03/2023", weight: "40", startReal:"", endReal:"", status:"fatto", onTime: true},
   ]
+}
+
+progress = 0;
+
+ngOnInit(): void {
+  this.calculateProgress();
+}
+
+calculateProgress() {
+  let doneGoals = 0;
+  let counter = 0;
+  this.obj.phases.map((elem: any) => {
+    counter += 1;
+    if (elem.status === "fatto") {doneGoals += 1};
+  });
+  this.progress = (doneGoals * 100 ) / counter
 }
 
 
