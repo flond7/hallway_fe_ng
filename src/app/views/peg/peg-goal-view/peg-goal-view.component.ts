387e79861442150ff5e68d36bd91dc7c14987783
flond7 <elisa.utilivenza@gmail.com>
2023-03-24 12:49:40 +0100

    reset progress value

--- src/app/views/peg/peg-goal-view/peg-goal-view.component.ts
+++ src/app/views/peg/peg-goal-view/peg-goal-view.component.ts
@@ -24,42 +24,46 @@ obj = {
   phases: [
     {description: "Studio della composizione del Piano e stesura parte relativa al Piano Nazionale Anticorruzione", startPrevision: "01/01/2023", endPrevision:"01/01/2023", weight: "40", startReal:this.todayDate, endReal:this.todayDate, status:"", onTime: true},
     {description: "Redazione parte relativa al Piano della Performance e piano della formazione", startPrevision: this.todayDate, endPrevision:this.todayDate, weight: "40", startReal: this.todayDate, endReal:this.todayDate, status:"", onTime: true},
-    {description: "Predisposizione atto finale e approvazione in Giunta. Successivi adempimenti.", startPrevision: this.todayDate, endPrevision:this.todayDate, weight: "40", startReal:this.todayDate, endReal:this.todayDate, status:"", onTime: true},
+    {description: "Predisposizione atto finale e approvazione in Giunta. Successivi adempimenti.", startPrevision: this.todayDate, endPrevision:this.todayDate, weight: "20", startReal:this.todayDate, endReal:null, status:"", onTime: true},
   ]
 }
 
 ngOnInit(): void {  
-  this.calculateProgress();
   this.obj.phases.map((phase:any) => {
     this.onTime(phase);
   });
+  
+  this.calculateProgress();
 }
 
 calculateProgress() {
-  let doneGoals = 0;
-  let counter = 0;
+  //let doneGoals = 0;
+  let doneGoals = this.obj.phases.length;
+  let counter = this.obj.phases.length;
+
+  this.obj.phases.map((elem: any) => {
+    if (elem.status === "Da fare") {doneGoals -= 1};
+    console.log(counter);
+    console.log(doneGoals);
+  });
+  /* let counter = 0;
   this.obj.phases.map((elem: any) => {
     counter += 1;
     if (elem.status === "fatto") {doneGoals += 1};
-  });
+  }); */
   this.progress = (doneGoals * 100 ) / counter
 }
 
 onTime(phase: any) {
-  //to test
-  phase.endReal = phase.startReal = phase.startPrevision = phase.endPrevision = this.todayDate;
-  if (phase.endReal > phase.endPrevision) {
-    console.log("ok")
+  if (phase.endReal === null) {
+    phase.status = "Da fare"
+  } else if (phase.endReal > phase.endPrevision) {
     phase.status = "In ritardo";
-  } else if (phase.endReal < phase.endPrevision) {
+  } else if (phase.endReal < phase.endPrevision ) {
     phase.status = "In anticipo";
   } else if (phase.endReal = phase.endPrevision) {
     phase.status = "In tempo";
-  } else {
-    phase.status = "Da fare"
   }
-
-
 }
 
 
