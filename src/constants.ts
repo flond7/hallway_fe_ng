d684738a3b7d346e0e22bc5a25ede41079885df5
flond7 <elisa.utilivenza@gmail.com>
2023-04-28 19:34:56 +0200

    added info

--- src/constants.ts
+++ src/constants.ts
@@ -6,3 +6,7 @@
 export const GOAL_WEIGHT: Array<object> = [{text: 'Facile', value: 1}, {text: 'Media difficoltà', value: 2}, {text: 'Alta difficoltà', value: 3},];
 export const GOAL_TYPE: Array<string> = ['Ordinario', 'Straordinario'];
 export const MAXIMUM_NUMBER = 10;
+
+
+export const INFO_GOAL_TYPE = "Gli obbiettivi possono essere ordinari, in questo caso hanno degli indicatori (es: numero di pratiche edilizie, numero di accessi agli atti...), oppure straordinari (es: preparazione e approvazione di un nuovo regolamento, gestione di bando straordinario...)";
+export const INFO_GOAL_PHASE = "";
