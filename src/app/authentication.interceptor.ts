841f3321c97b181eaffbf7a29c02895a646df4d7
flond7 <elisa.utilivenza@gmail.com>
2023-10-26 13:54:39 +0200

    interceptor no luck

--- src/app/authentication.interceptor.ts
+++ src/app/authentication.interceptor.ts
@@ -12,27 +12,44 @@ import { AuthService } from './services/auth.service'
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     // custom header to eventyally skip interceptor if this voice is true
-    if (request.headers.has('Skip-Interceptor') && request.headers.get('Skip-Interceptor') === 'true') {
+    console.log('interceptor')
+    console.log(request.headers.get('Skip-Interceptor'))
+    /* if (request.headers.has('Skip-Interceptor') && request.headers.get('Skip-Interceptor') === 'true') {
+      console.log('skip')
       return next.handle(request);
-    }
+    } */
+
     // get the JWT token
     const token = this.authService.getToken();
-    // get the CSRF token
-    const csrfToken = this.authService.getCSRFToken();
-    
-    // Clone the request and add the token to the headers
+
+    // get the JWT token
+    const csrfToken = this.authService.getCSRFToken().subscribe(r => console.log(r));
+
+    //clone the request and add the CSRF token
+    const csrfModifiedRequest = request.clone({
+      setHeaders: {
+        'X-CSRFToken': csrfToken as unknown as string                //type assertion because TS is expecting a string and receives an object
+      }
+    });
+
+    return next.handle(csrfModifiedRequest);
+
+  }
+}
+
+    /* // Clone the request and add the token to the headers
     const modifiedRequest = request.clone({
       setHeaders: {
         'Authorization': `Bearer ${token}`,
       }
-    });
+    }); */
 
     // Now, get the CSRF token and add it to the headers
-    return this.authService.getCSRFToken().pipe(
+    /* return this.authService.getCSRFToken().pipe(
       switchMap(csrfToken => {
         const csrfModifiedRequest = modifiedRequest.clone({
           setHeaders: {
@@ -41,6 +58,31 @@ export class AuthenticationInterceptor implements HttpInterceptor {
         });
         return next.handle(csrfModifiedRequest);
       })
+    ); */
+
+
+
+/*   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    // get the JWT token
+    const token = this.authService.getToken();
+
+    // Clone the request and add the JWT token to the Authorization header
+    const modifiedRequest = request.clone({
+      setHeaders: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+
+    // Now, get the CSRF token and add it to the headers
+    return this.authService.getCSRFToken().pipe(
+      switchMap(csrfToken => {
+        const csrfModifiedRequest = modifiedRequest.clone({
+          setHeaders: {
+            'X-CSRFToken': csrfToken
+          }
+        });
+        return next.handle(csrfModifiedRequest);
+      })
     );
   }
-}
+} */
