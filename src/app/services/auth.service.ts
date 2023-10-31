8d196c5bd6c0e42e6df898469d4a8b0da2dd5a0e
flond7 <elisa.utilivenza@gmail.com>
2023-10-31 10:48:45 +0100

    test

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { BASE_URL, HTTP_OPTIONS} from '../../constants'
-import { tap, catchError, mergeMap } from 'rxjs/operators';
+import { Observable, of, throwError } from 'rxjs';
+import { BASE_URL, HTTP_OPTIONS } from '../../constants'
+import { tap, catchError, mergeMap, switchMap, map } from 'rxjs/operators';
+
+import { CookieService } from 'ngx-cookie-service';
 
 
 const baseURL = BASE_URL;
@@ -16,75 +18,161 @@ export class AuthService {
   private authJWToken: string = "";
   private csrfToken: string = "";
 
+
+  constructor(private http: HttpClient, private cookieService: CookieService) { }
+  /* 
+    getCookie(name: string): string | null {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) {
+        const cookieValue = parts.pop()?.split(';').shift();
+        return cookieValue || null; // Return null if the cookie value is falsy
+      }
+      return null; // Return null if the cookie is not found
+    }
+   */
+
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
       'Cache-Control': 'no-cache',
+      //'X-CSRFToken': this.csrfToken, // Access csrfToken within the object's properties.
     }),
   };
 
-  constructor(private http: HttpClient) {}
-
-  getCSRFToken() {
+  /* getCSRFToken(): Observable<any> {
+    const cookieValue = this.cookieService.get('csrftoken');
+    console.log(cookieValue);
     return this.http.post(baseURL + 'common/get_csrf_token', this.httpOptions)
   }
+ */
 
-  login(username: string, password: string): Observable<any> {
-    console.log('login auth service');
-    
-    //retrieve single apps authorization
-    //let userid = 2;
-    
-/*     this.getAuthorizations(userid).subscribe(r=> {
-      // Save variables to local storage
-      Object.keys(r.data).forEach(key => {
-        const value = r.data[key];
-        localStorage.setItem(key, JSON.stringify(value));
-      });
-    }); */
-    
-    // First, get the CSRF token and update the headers
-    return this.getCSRFToken().pipe(
-      mergeMap((r: any) => {
-        //console.log('CSRF Token Response:', r);
-        this.csrfToken = r.csrf_token;
-        document.cookie = 'csrftoken=' + this.csrfToken;
-        this.httpOptions.headers = this.httpOptions.headers.set('X-CSRFToken', this.csrfToken);
-        // Then, send the login request and return the observable
-        return this.http.post(baseURL + 'common/user_log', { username, password }, this.httpOptions).pipe(
-
-          catchError((error: any) => {
-            // Handle the error here, and optionally, log it
-            //console.error('Login failed:', error);
-            // You can also customize the error message or response before re-throwing it
-            return throwError(() => error);    
-          })
-        );
+/* 
+  getCSRFToken(): Observable<string> {
+    return this.http.get<string>(baseURL + 'common/get_csrf_token',  { withCredentials: true }).pipe(
+      switchMap(r => {console.log(r))}
+    )
+  }
+ */
+  getCSRFToken(): Observable<string> {
+    return this.http.get<string>(baseURL + 'common/get_csrf_token', { withCredentials: true }).pipe(
+      map(response => {
+        //const csrfToken = response.csrftoken; // Adjust this to match your API response structure
+        //console.log('CSRF token:', csrfToken);
+        console.log(response)
+        return response;
       })
     );
   }
   
-  getAuthorizations(pk:number): Observable<any> {
-    return this.http.get(baseURL + 'common/get_user_profiles_auth/'+pk, this.httpOptions)
+
+  login(username: string, password: string): Observable<any> {
+    // Get the CSRF token from the cookie
+    const csrfToken = this.cookieService.get('csrftoken');
+    console.log(csrfToken);
+
+    console.log(document.cookie.split(';'))
+      
+  
+    
+
+
+
+
+
+
+  
+    // Prepare the data for the login request
+    const loginData = {
+      username: username,
+      password: password,
+      csrfmiddlewaretoken: csrfToken // Include the CSRF token in the request
+    };
+  
+    // Send the POST request for login
+    return this.http.post(baseURL + 'common/user_log', { username, password }, this.httpOptions)
+  }
+  
+
+ /*  getCSRFToken(): Observable<string> {
+    return this.http.get(baseURL + 'common/get_csrf_token', {}, this.httpOptions).pipe(
+      switchMap((response: any) => {
+        if (response && response.csrf_token) {
+          return of(response.csrf_token); // Return the CSRF token as an observable
+        }
+        return of('default_csrf_token'); // You can provide a default token or handle errors as needed
+      })
+    );
+  } */
+
+
+
+
+
+  /* login(username: string, password: string): Observable<any> {
+    console.log('login auth service');
+    return this.getCSRFToken().pipe(
+      switchMap((response: any) => {
+        //const cookieValue = this.cookieService.get('csrftoken');
+        //console.log('cookie'+cookieValue);
+        console.log('rrivato')
+        return of('cookieValue')
+        // Now you can use the CSRF token (csrfToken) and the cookie (cookieValue) here
+        // Perform your login logic
+        // Example: return this.http.post(loginUrl, { username, password, csrfToken });
+      })
+    );
+  } */
+
+
+
+
+
+  // Return the merged observable
+  /* return this.getCSRFToken().pipe(
+  mergeMap((r: any) => {
+    this.httpOptions.headers = this.httpOptions.headers.set('X-CSRFToken', r.csrf_token);
+    console.log(this.httpOptions);
+    console.log(r.csrf_token);
+    return r */
+  // Send the login request and return the observable
+  /* return this.http.post(baseURL + 'common/user_log', { username, password }, this.httpOptions)
+    .pipe(
+      tap(response => console.log(response)),
+      catchError(error => {
+        console.error('Error:', error);
+        return throwError(error); // Re-throw the error to propagate it to the calling code.
+      }),
+    );  */
+  /*     })
+    );
+    } */
+
+
+
+
+
+  getAuthorizations(pk: number): Observable<any> {
+    return this.http.get(baseURL + 'common/get_user_profiles_auth/' + pk, this.httpOptions)
   }
 
-/* 
-    return this.http.post(baseURL + 'accounts/login/', { username, password }, this.httpOptions)
-      .pipe(
-        tap(response => console.log(response)),
-        catchError(error => {
-          console.error('Error:', error);
-          return throwError(error); // Re-throw the error to propagate it to the calling code.
-        })
-         tap(response => {
-          console.log(response);
-          const token = response.headers.get('Authorization');
-          if (token) {
-            this.storeToken(token);
-          }
-        }) 
-      ); */
-  
+  /* 
+      return this.http.post(baseURL + 'accounts/login/', { username, password }, this.httpOptions)
+        .pipe(
+          tap(response => console.log(response)),
+          catchError(error => {
+            console.error('Error:', error);
+            return throwError(error); // Re-throw the error to propagate it to the calling code.
+          })
+           tap(response => {
+            console.log(response);
+            const token = response.headers.get('Authorization');
+            if (token) {
+              this.storeToken(token);
+            }
+          }) 
+        ); */
+
 
   private storeToken(token: string) {
     // Store the token securely, such as in local storage, session storage, or as an HttpOnly cookie
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,36 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service'
 import { ModalService } from '../../services/modals.service';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.sass']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   username: string = '';
   password: string = '';
 
-  constructor(private router: Router, private authService: AuthService, public modalService: ModalService) {}
+  constructor(private router: Router, private authService: AuthService, public modalService: ModalService, private cookieService: CookieService) { }
+
+  ngOnInit() {
+        //ask for CSRF token
+    this.authService.getCSRFToken().subscribe(r => {
+      console.log('cookie ok')
+      const csrfToken = this.cookieService.get('csrf_token');
+      console.log(csrfToken);
+    })
+  }
 
   login() {
     this.authService.login(this.username, this.password).subscribe(
       (r) => {
         console.log(r)
-      if (r.status == 201) {
-        this.authService.getAuthorizations(r.userid).subscribe(r=> {
-          // Save variables to local storage
-          Object.keys(r.data).forEach(key => {
-            const value = r.data[key];
-            localStorage.setItem(key, JSON.stringify(value));
-          });
-          this.router.navigate(['/home']);
-        });;
-        //redirect to home
-        //this.router.navigate(['/home']);
-      }},
+        if (r.status == 201) {
+          this.authService.getAuthorizations(r.userid).subscribe(r => {
+            // Save variables to local storage
+            Object.keys(r.data).forEach(key => {
+              const value = r.data[key];
+              localStorage.setItem(key, JSON.stringify(value));
+            });
+            this.router.navigate(['/home']);
+          });;
+          //redirect to home
+          //this.router.navigate(['/home']);
+        }
+      },
       (error) => {
         console.log(error);
         let data = "GENERICO: credenziali non valide";
