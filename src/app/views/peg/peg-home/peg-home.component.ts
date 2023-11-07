8eb2bd6d4bc80f4cb16fd8af999ff992cec41565
flond7 <elisa.utilivenza@gmail.com>
2023-11-07 10:34:23 +0100

    basic home with office list

--- src/app/views/peg/peg-home/peg-home.component.ts
+++ src/app/views/peg/peg-home/peg-home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PegApiService } from '../../../services/peg-api.service';
+import { PegOffice } from 'src/interfaces';
+import * as GC from '../../../../constants'
 
 @Component({
   selector: 'app-peg-home',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./peg-home.component.sass']
 })
 export class PegHomeComponent {
+
+  namePA: string = GC.NAME_PA;
+
+  // selected user for reasearch
+  officeList$: PegOffice[] = []
+
+
+  //year
+  year: number = 0;
+
+  constructor(private api: PegApiService) {
+      const currentDate = new Date();
+      this.year = currentDate.getFullYear();
+      api.officeListData$.subscribe(r => { this.officeList$ = r; console.log(this.officeList$) });
+    }
 
 }
