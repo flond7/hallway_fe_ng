09725b86845f8f60bf2ced2b02bdabc8584dc04d
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2023-11-11 19:17:52 +0100

    added new graphic header and modal

--- src/app/views/peg/peg-new-goal/peg-new-goal.component.ts
+++ src/app/views/peg/peg-new-goal/peg-new-goal.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import * as GC from '../../../../constants'
+import { PegPerson, PegOffice } from 'src/interfaces';
+import { PegApiService } from '../../../services/peg-api.service';
+import * as GC from '../../../../constants';
+// Modal imports
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ModalService } from '../../../services/modals.service'
 
 
 @Component({
@@ -9,11 +14,29 @@ import * as GC from '../../../../constants'
 })
 export class PegNewGoalComponent {
 
+  year: number = 0;
+
+  //Office selection
+  officeList$: PegOffice[] = [];
+  selectedOffice: PegOffice = {
+    id: 0,
+    name: 'Seleziona un ufficio'
+  };
+
+  modalRefOffice!: BsModalRef;
+
   // tab control
   extraordinary: boolean = true;
   extraordinaryTitle: string = GC.PEG_GOAL_EXTRAORDINARY_TITLE;
   ordinaryTitle: string = GC.PEG_GOAL_ORDINARY_TITLE;
 
-  constructor() { }
+  constructor(private api: PegApiService, private modalService: ModalService, public bsModalService: BsModalService) {
+    const currentDate = new Date();
+    this.year = currentDate.getFullYear();
+
+    api.officeListData$.subscribe(r => {
+      this.officeList$ = r;
+    })
+  }
 
 }
