064fa5879b066055cf326453926e3e67f975a648
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2023-11-11 22:12:40 +0100

    new graphic for add goal

--- src/app/views/peg/peg-new-goal/peg-new-goal.component.ts
+++ src/app/views/peg/peg-new-goal/peg-new-goal.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { PegPerson, PegOffice } from 'src/interfaces';
+import { PegPerson, PegOffice, PegGoal } from 'src/interfaces';
 import { PegApiService } from '../../../services/peg-api.service';
 import * as GC from '../../../../constants';
 // Modal imports
@@ -16,7 +16,7 @@ export class PegNewGoalComponent {
 
   year: number = 0;
 
-  //Office selection
+  // Office selection
   officeList$: PegOffice[] = [];
   selectedOffice: PegOffice = {
     id: 0,
@@ -30,6 +30,24 @@ export class PegNewGoalComponent {
   extraordinaryTitle: string = GC.PEG_GOAL_EXTRAORDINARY_TITLE;
   ordinaryTitle: string = GC.PEG_GOAL_ORDINARY_TITLE;
 
+  // Css control
+  primarypeg = GC.COLOR_PRIMARY_PEG;
+  dark = GC.COLOR_DARK;
+  white1 = GC.COLOR_WHITE_ONE;
+  white2 = GC.COLOR_WHITE_TWO;
+  white3 = GC.COLOR_WHITE_THREE;
+
+  // Goal list
+  goalList: PegGoal[] = [];
+
+  // total calculation
+  extraordinaryTotalWeight: number = 0;
+  extraordinaryWeight3006: number = 0;
+  extraordinaryWeight3112: number = 0;
+  ordinaryTotalWeight: number = 0;
+  ordinaryWeight3006: number = 0;
+  ordinaryWeight3112: number = 0;
+
   constructor(private api: PegApiService, private modalService: ModalService, public bsModalService: BsModalService) {
     const currentDate = new Date();
     this.year = currentDate.getFullYear();
@@ -37,6 +55,24 @@ export class PegNewGoalComponent {
     api.officeListData$.subscribe(r => {
       this.officeList$ = r;
     })
+  }
+
+
+  getTotals(list: PegGoal[]) {
+    const weight = list.reduce((sum, goal) => sum + goal.weight, 0);
+    const weight3006 = list.reduce((sum, goal) => {
+      if (goal.weight_3006 !== undefined) {
+        return sum + goal.weight_3006;
+      }
+      return sum;
+    }, 0)
+    const weight3112 = list.reduce((sum, goal) => {
+      if (goal.weight_3112 !== undefined) {
+        return sum + goal.weight_3112;
+      }
+      return sum;
+    }, 0)
+    return [weight, weight3006, weight3112]
   }
 
 }
