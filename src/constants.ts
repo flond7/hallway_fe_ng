7bbf019c1ea4e3e93c325508d1511f0efe7a8a4f
flond7 <elisa.utilivenza@gmail.com>
2023-11-15 14:10:39 +0100

    added color

--- src/constants.ts
+++ src/constants.ts
@@ -15,6 +15,7 @@ export const COLOR_WHITE: string = '#ffffff';
 export const COLOR_WHITE_ONE: string = 'rgba(255,255,255,0.8)';
 export const COLOR_WHITE_TWO: string = 'rgba(255,255,255,0.4)';
 export const COLOR_WHITE_THREE: string = 'rgba(255,255,255,0.2)';
+export const COLOR_DARK_ONE: string = 'rgba(25,27,35,1)';
 
 //MODALS
 export const MODAL_DELETE: string = 'Vuoi davvero cancellare questo elemento?';
