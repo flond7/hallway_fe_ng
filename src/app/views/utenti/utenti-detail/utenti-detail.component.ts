cf41d809a419304d95d1e2539f8aff3cdf5b6c99
flond7 <elisa.utilivenza@gmail.com>
2024-01-04 09:30:57 +0100

    graphic structure

--- src/app/views/utenti/utenti-detail/utenti-detail.component.ts
+++ src/app/views/utenti/utenti-detail/utenti-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { UtentiApiService } from '../../../services/utenti-api.service';
 import { PegPerson } from 'src/interfaces';
+
+import { faEnvelope, faFolderTree, faHatWizard } from '@fortawesome/free-solid-svg-icons';
 import * as GC from '../../../../constants'
 
 @Component({
@@ -10,6 +12,9 @@ import * as GC from '../../../../constants'
 })
 export class UtentiDetailComponent {
 
+  // FA icons
+  faEnvelope = faEnvelope; faFolderTree = faFolderTree; faHatWizard = faHatWizard
+
   // string and messages
   namePA: string = GC.NAME_PA;
 
@@ -18,23 +23,23 @@ export class UtentiDetailComponent {
   surname = "Pessa";
   office = "Servizio informatico";
 
-  credentials = {}
-
 
   elisa: PegPerson = {
     id: 1,
-    name: 'elisa',
-    surname: 'pessa',
-    jobCategory: 'D1',
+    name: '',
+    surname: '',
+    jobCategory: '',
     manager: false,
     managerOfOffices: [],
 }
+
+credentials: any;
 
   constructor(private api: UtentiApiService,) {}
 
   getUserCredentials(user: PegPerson) {
     this.api.getUserCredentials(user).subscribe(r => {
-      this.credentials = r.data;
+      this.credentials = r.data[0];
       console.log(this.credentials)
     })
   }
